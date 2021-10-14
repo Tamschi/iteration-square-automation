2bd7671e7fe69e81069e9c3d883230e6f4c11cd8
Tamme Schichler <tamme@schichler.dev>
2021-10-14 18:51:41 +0200

    Use `net/http.StatusOK` in `health_check` function

--- functions/health_check/main.go
+++ functions/health_check/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -9,7 +10,7 @@ import (
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
+		StatusCode: http.StatusOK,
 	}, nil
 }
 
